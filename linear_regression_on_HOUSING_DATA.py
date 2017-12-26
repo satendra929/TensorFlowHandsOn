07f8611b655e4884957a535dbd1e9b6db7d69174
satendra929 <satendra929@gmail.com>
2017-12-26 04:58:37 -0500

    Linear Regression on Housing Data

--- linear_regression_on_HOUSING_DATA.py
+++ linear_regression_on_HOUSING_DATA.py
@@ -2,15 +2,28 @@ import tensorflow as tf
 import numpy
 import random
 import matplotlib.pyplot as plt
+import csv
 
-alpha=0.01
+alpha=0.0000001
 cycles=100
 
-a=[1,2,3,4,5,6,7,8,9,10]
-b=[1,2,3,3,2,6,7,4,9,10]
-train_x=numpy.asarray(a)
-train_y=numpy.asarray(b)
+prices=[]
+sqft=[]
+first=True
+with open('RealEstate.csv','r') as f :
+    reader=csv.reader(f)
+    for row in reader :
+        if first==True :
+            first=False
+        else :
+            prices.append((int)(row[2][:-3]))
+            sqft.append((int)(row[5]))
+
+train_x=numpy.asarray(sqft)
+train_y=numpy.asarray(prices)
 n=numpy.size(train_x)
+
+print(n)
 
 W=tf.Variable(numpy.random.randn(),name="weight")
 b=tf.Variable(numpy.random.randn(),name="bias")
@@ -20,7 +33,7 @@ Y = tf.placeholder("float")
 
 out=tf.add(tf.multiply(X,W),b)
 
-error=tf.reduce_sum(tf.pow(out-Y,2))
+error=tf.reduce_sum(tf.pow(out-Y,2))/(2*n)
 gd=tf.train.GradientDescentOptimizer(alpha).minimize(error)
 
 init=tf.global_variables_initializer()
@@ -39,3 +52,5 @@ plt.plot(train_x, train_y, 'ro', label='Data')
 plt.plot(train_x, sess.run(W) * train_x + sess.run(b), label='Model')
 plt.legend()
 plt.show()
+
+
